252162ca3c87984992b67f3e93fb395e18318c11
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-01-05 00:48:45 -0600

    feat: add `Inter` font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,25 @@
 import '@/globals.css.ts';
 
+import { Inter } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+
+const inter = Inter({
+  display: 'swap',
+  subsets: ['latin'],
+});
 
 function NextApp(props: AppProps<Record<string, unknown>>): JSX.Element {
   const { Component, pageProps } = props;
 
   return (
-    <>
+    <div className={inter.className}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
 
