3c3279f76963255bb6e5baeff1db69e2d78fd606
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-01-04 22:50:18 -0600

    chore: update Next.js config file

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
