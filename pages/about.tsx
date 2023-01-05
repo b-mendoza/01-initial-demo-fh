59627c7ae26f30130d1a14576947ce54820bd64e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-01-04 22:52:07 -0600

    feat: add `About` page

---
+++ pages/about.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Link from "next/link";
+
+function AboutPage(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>About | Bryan Mendoza</title>
+        <meta name="description" content="About Page" />
+      </Head>
+
+      <main>
+        <h1>
+          Ir a <Link href="/">Home</Link>
+        </h1>
+      </main>
+    </>
+  );
+}
+
+export default AboutPage;
