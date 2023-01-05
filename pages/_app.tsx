b4607bf883a1db2c5210eabfa3364bd64c773c22
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-01-04 22:51:48 -0600

    refactor: format _app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+function NextApp(props: AppProps<Record<string, unknown>>): JSX.Element {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default NextApp;
