0bd1e525850f6e7b65304c6445ffe10b3731dc48
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-01-05 03:58:53 -0600

    refactor: update `IndexPage` component

--- initial-demo-fh/pages/index.tsx
+++ initial-demo-fh/pages/index.tsx
@@ -1,9 +1,10 @@
+import type { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 
 import Navbar from '@/components/Navbar';
 
-function IndexPage(): JSX.Element {
+function IndexPage(props: unknown): JSX.Element {
   return (
     <>
       <Head>
@@ -19,9 +20,19 @@ function IndexPage(): JSX.Element {
         <h1>
           Go to <Link href="/about">About</Link>
         </h1>
+
+        <pre>{JSON.stringify(props, null, 2)}</pre>
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return await Promise.resolve({
+    props: {
+      apiUrl: process.env['API_URL'] ?? '',
+    },
+  });
+};
 
 export default IndexPage;
